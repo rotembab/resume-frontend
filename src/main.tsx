b5d8cf331723d7766d57d74e70dc7c56ba8ce654
Rotem Babani <rotembabmen@gmail.com>
2025-01-29 12:10:56 +0200

    added custom palette and theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { ThemeProvider } from '@emotion/react';
+import { AppTheme } from './theme.ts';
+import { CssBaseline } from '@mui/material';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider theme={AppTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
---
+++ src/theme.ts
@@ -0,0 +1,37 @@
+import { createTheme } from '@mui/material/styles';
+
+declare module '@mui/material/styles' {
+  interface Palette {
+    customColors: {
+      darkest: string;
+      heading: string;
+      paragraph: string;
+    };
+  }
+  interface PaletteOptions {
+    customColors?: {
+      darkest?: string;
+      heading?: string;
+      paragraph?: string;
+    };
+  }
+}
+export const AppTheme = createTheme({
+  palette: {
+    mode: 'dark',
+
+    customColors: {
+      darkest: '#1C1A19',
+      heading: '#353334',
+      paragraph: '#998e8f',
+    },
+
+    primary: {
+      main: '#ff0000', //red primary color
+    },
+
+    background: {
+      default: '#161312', //background color black
+    },
+  },
+});
