fcf4c9298f37d304a452b35fe9bd94d59f871284
Rotem Babani <rotembabmen@gmail.com>
2025-02-02 12:10:13 +0200

    added proper colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,3 @@
-import { Button } from '@mui/material';
-
 export default function App() {
-  return (
-    <>
-      <Button color='lightBlack'>Light Black Button</Button>
-      <Button color='darkGrey'>Dark Grey Button</Button>
-      <Button color='lightGrey'>Light Grey Button</Button>
-    </>
-  );
+  return <></>;
 }
--- src/themes/custom-colors.ts
+++ src/themes/custom-colors.ts
@@ -1,7 +1,11 @@
 export const customColors = {
-  lightBlack: { main: '#1C1A19', contrastText: '#fff' },
-  darkGrey: { main: '#353334', contrastText: '#fff' },
-  lightGrey: { main: '#998e8f', contrastText: '#000' },
+  background: { main: '#151412', contrastText: '#fff' },
+  hoverColor: { main: '#1E1D1C', contrastText: '#fff' },
+  headingDarkColor: { main: '#3B393A', contrastText: '#fff' },
+  headingLightColor: { main: '#FFFFFF', contrastText: '#000' },
+  descriptionColor: { main: '#7A7B7D', contrastText: '#fff' },
+  paragraphColor: { main: '#998F8F', contrastText: '#fff' },
+  primeColor: { main: '#F46C38', contrastText: '#fff' },
 };
 
 export type CustomColor = keyof typeof customColors;
