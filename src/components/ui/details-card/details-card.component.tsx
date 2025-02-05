d23f40cbcf835325af32aae7638ea618d1321905
Rotem Babani <rotembabmen@gmail.com>
2025-02-05 11:59:39 +0200

    grid is now working properly

--- src/components/ui/details-card/details-card.component.tsx
+++ src/components/ui/details-card/details-card.component.tsx
@@ -1,7 +1,7 @@
 import { Box, Button, Card, Typography } from '@mui/material';
 import { useLocation } from 'react-router';
 import { SlideFadeTransition } from '../slide-fade-transition/slide-fade-transition-component';
-
+import Grid from '@mui/material/Grid2';
 import { detailsCardLinks } from './details-card-links.config';
 import { useTranslation } from 'react-i18next';
 
@@ -31,33 +31,43 @@ export const DetailsCard = () => {
           padding: '1rem',
         }}
       >
-        <img
-          style={{ borderRadius: '1rem' }}
-          width={200}
-          height={300}
-          src='https://placehold.co/400x600'
-          alt='placeholder'
-        />
-        <Typography variant='h3'>{t('Card.name')}</Typography>
-        <Typography
-          color='descriptionColor'
-          variant='caption'
-          sx={{
-            fontWeight: 500,
-            fontSize: {
-              xs: '18px',
-            },
-          }}
-        >
-          {t('Card.description')}
-        </Typography>
-        <Box>
-          {detailsCardLinks.map((item, index) => (
-            <Button key={item.link} href={item.link} target='_blank'>
-              {item.icon}
-            </Button>
-          ))}
-        </Box>
+        <Grid container rowSpacing={2}>
+          <Grid size={12}>
+            <img
+              style={{ borderRadius: '1rem' }}
+              width={200}
+              height={300}
+              src='https://placehold.co/400x600'
+              alt='placeholder'
+            />
+          </Grid>
+          <Grid size={12}>
+            <Typography variant='h3'>{t('Card.name')}</Typography>
+          </Grid>
+          <Grid size={12}>
+            <Typography
+              color='descriptionColor'
+              variant='caption'
+              sx={{
+                fontWeight: 500,
+                fontSize: {
+                  xs: '18px',
+                },
+              }}
+            >
+              {t('Card.description')}
+            </Typography>
+          </Grid>
+          <Grid size={12}>
+            <Box>
+              {detailsCardLinks.map((item, index) => (
+                <Button key={item.link} href={item.link} target='_blank'>
+                  {item.icon}
+                </Button>
+              ))}
+            </Box>
+          </Grid>
+        </Grid>
       </Card>
     </SlideFadeTransition>
   );
