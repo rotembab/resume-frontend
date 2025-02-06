669c50f3046568f2a19cd7acd51d2837a2cae5ba
Rotem Babani <rotembabmen@gmail.com>
2025-02-06 17:11:36 +0200

    removed render when visible due to bugs

--- src/components/pages/contact-me/contact-me.component.tsx
+++ src/components/pages/contact-me/contact-me.component.tsx
@@ -6,39 +6,39 @@ import { useTranslation } from 'react-i18next';
 import { ContactForm } from './contact-form.component';
 
 export const ContactMe = () => {
-  const { ref, inView } = useInView({
-    triggerOnce: true, // Ensures the component only loads once when in view
-    threshold: 1, // Adjust based on when you consider the component "visible"
-  });
+  // const { ref, inView } = useInView({
+  //   triggerOnce: true, // Ensures the component only loads once when in view
+  //   threshold: 1, // Adjust based on when you consider the component "visible"
+  // });
   const { t } = useTranslation();
   return (
-    <div ref={ref}>
-      {inView && (
-        <SlideFadeTransition
-          fadeAppear
-          fadeIn
-          slideAppear
-          slideIn
-          slideKey={'slide'}
-          fadeKey={'fade'}
-          slideTimeout={{ enter: 500 }}
-          fadeStyle={{
-            transitionDuration: '1s',
-          }}
-        >
-          <Grid container>
-            <Grid size={12}>
-              <Typography variant='h1'>{t('Contact.heading1')}</Typography>
-              <Typography color='headingDarkColor' variant='h1'>
-                {t('Contact.heading2')}
-              </Typography>
-            </Grid>
-            <Grid size={12}>
-              <ContactForm />
-            </Grid>
-          </Grid>
-        </SlideFadeTransition>
-      )}
-    </div>
+    // <div ref={ref}>
+    //   {inView && (
+    <SlideFadeTransition
+      fadeAppear
+      fadeIn
+      slideAppear
+      slideIn
+      slideKey={'slide'}
+      fadeKey={'fade'}
+      slideTimeout={{ enter: 500 }}
+      fadeStyle={{
+        transitionDuration: '1s',
+      }}
+    >
+      <Grid container>
+        <Grid size={12}>
+          <Typography variant='h1'>{t('Contact.heading1')}</Typography>
+          <Typography color='headingDarkColor' variant='h1'>
+            {t('Contact.heading2')}
+          </Typography>
+        </Grid>
+        <Grid size={12}>
+          <ContactForm />
+        </Grid>
+      </Grid>
+    </SlideFadeTransition>
+    // )}
+    // </div>
   );
 };
--- src/components/pages/home/home.page.tsx
+++ src/components/pages/home/home.page.tsx
@@ -11,6 +11,15 @@ export const HomePage = () => {
       <Grid size={12}>
         <ContactMe />
       </Grid>
+      <Grid size={12}>
+        <ContactMe />
+      </Grid>
+      <Grid size={12}>
+        <ContactMe />
+      </Grid>
+      <Grid size={12}>
+        <ContactMe />
+      </Grid>
     </Grid>
   );
 };
